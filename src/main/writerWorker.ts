cf9956416c12958593faad3a3d35c7cac1b4f10d
gemrey13 <gemreyranola@gmail.com>
2026-02-11 16:41:14 +0800

    change replace to upsert

--- src/main/writerWorker.ts
+++ src/main/writerWorker.ts
@@ -13,7 +13,7 @@ db.exec(`
 
 // Prepared statement
 const insertStmt = db.prepare(`
-  INSERT OR REPLACE INTO pos_transactions (
+  INSERT INTO pos_transactions (
     branch, branch_name, cslipno, orddate, ordtime,
     cusno, cusname, cusaddr1, cusaddr2, custel, cusfax, cuscont, age,
     chargpct, grschrg, promo_pct, promo_amt, sr_tcust, sr_body, sr_disc,
@@ -32,7 +32,63 @@ const insertStmt = db.prepare(`
     @dis_h, @dis_i, @dis_j, @dis_k, @dis_l, @dis_vx,
     @terms, @cardno, @cardtyp, @lastpd, @remarks, @filler1, @filler2
   )
+  ON CONFLICT(branch, cslipno)
+  DO UPDATE SET
+    branch_name = excluded.branch_name,
+    orddate = excluded.orddate,
+    ordtime = excluded.ordtime,
+    cusno = excluded.cusno,
+    cusname = excluded.cusname,
+    cusaddr1 = excluded.cusaddr1,
+    cusaddr2 = excluded.cusaddr2,
+    custel = excluded.custel,
+    cusfax = excluded.cusfax,
+    cuscont = excluded.cuscont,
+    age = excluded.age,
+    chargpct = excluded.chargpct,
+    grschrg = excluded.grschrg,
+    promo_pct = excluded.promo_pct,
+    promo_amt = excluded.promo_amt,
+    sr_tcust = excluded.sr_tcust,
+    sr_body = excluded.sr_body,
+    sr_disc = excluded.sr_disc,
+    vat = excluded.vat,
+    servchrg = excluded.servchrg,
+    othdisc = excluded.othdisc,
+    udisc = excluded.udisc,
+    bankcharg = excluded.bankcharg,
+    totchrg = excluded.totchrg,
+    pdamt = excluded.pdamt,
+    pmtdisc = excluded.pmtdisc,
+    balance = excluded.balance,
+    tcash = excluded.tcash,
+    tcharge = excluded.tcharge,
+    tsigned = excluded.tsigned,
+    vat_xmpt = excluded.vat_xmpt,
+    ntax_sal = excluded.ntax_sal,
+    dis_prom = excluded.dis_prom,
+    dis_udisc = excluded.dis_udisc,
+    dis_sr = excluded.dis_sr,
+    dis_emp = excluded.dis_emp,
+    dis_vip = excluded.dis_vip,
+    dis_gpc = excluded.dis_gpc,
+    dis_pwd = excluded.dis_pwd,
+    dis_g = excluded.dis_g,
+    dis_h = excluded.dis_h,
+    dis_i = excluded.dis_i,
+    dis_j = excluded.dis_j,
+    dis_k = excluded.dis_k,
+    dis_l = excluded.dis_l,
+    dis_vx = excluded.dis_vx,
+    terms = excluded.terms,
+    cardno = excluded.cardno,
+    cardtyp = excluded.cardtyp,
+    lastpd = excluded.lastpd,
+    remarks = excluded.remarks,
+    filler1 = excluded.filler1,
+    filler2 = excluded.filler2
 `)
+
 
 let totalInserted = 0
 
